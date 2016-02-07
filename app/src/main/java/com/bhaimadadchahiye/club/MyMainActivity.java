f9bb3fedfa35339574d9095d005a11551d7a9c46
Vishal Chandak <vishalchandak0212@gmail.com>
2016-02-07 18:24:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/bhaimadadchahiye/club/MyMainActivity.java
@@ -0,0 +1,9 @@
+package com.bhaimadadchahiye.club;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity to go if the login is successful
+ */
+public class MyMainActivity extends AppCompatActivity {
+}
