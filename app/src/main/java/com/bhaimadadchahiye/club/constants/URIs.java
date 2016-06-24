15fd377280c4e30d8e0b788367c7c5f703e7439b
Vishal Chandak <vishalchandak0212@gmail.com>
2016-06-24 13:05:15 +0530

    Update .gitignore

---
+++ app/src/main/java/com/bhaimadadchahiye/club/constants/URIs.java
@@ -0,0 +1,33 @@
+package com.bhaimadadchahiye.club.constants;
+
+/**
+ * URLs for various operations
+ */
+public class URIs {
+
+    public static String loginURL = "http://localhost/bhaiMadadChahiye/";
+
+    public static String registerURL = "http://localhost/bhaiMadadChahiye/";
+
+    public static String forpassURL = "http://localhost/bhaiMadadChahiye/";
+
+    public static String chgpassURL = "http://localhost/bhaiMadadChahiye/";
+
+    public static String locationUrl = "http://localhost/bhaiMadadChahiye/location/";
+
+    public static String questionUrl = "http://localhost/bhaiMadadChahiye/question/";
+
+    public static String login_tag = "login";
+
+    public static String register_tag = "register";
+
+    public static String forpass_tag = "forpass";
+
+    public static String chgpass_tag = "chgpass";
+
+    public static String home_location_tag = "homeLocation";
+
+    public static String post_question_tag = "postQuestion";
+
+    public static String load_questions = "aurDikhao";
+}
